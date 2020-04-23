c8b94b82c1c7cc2a3e1299e7ba31adac50711ea5
Harrison Price <hprice518@gmail.com>
2020-04-23 00:35:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   remove_array = ["a","b","c","d"]
-  remove_array.pop
+  d_array = remove_array.pop
 end
 
 def remove_element_from_start_of_array(array)
