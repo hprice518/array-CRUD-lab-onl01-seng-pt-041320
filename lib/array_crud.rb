9982eebbebbaeba93cae79531a07fdfc40291bd5
Harrison Price <hprice518@gmail.com>
2020-04-23 00:22:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  ["Mona Lisas" , "Mad Hatters" , "Sons of Bankers" , "Sons of Lawyers"]
 end
 
 def add_element_to_end_of_array(array, element)
