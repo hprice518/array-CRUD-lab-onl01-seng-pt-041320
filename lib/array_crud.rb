ba3a8d4916bf5a3c4beed845143ab5ceec343f6a
Harrison Price <hprice518@gmail.com>
2020-04-23 00:39:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  retrieve_array = ["wow","zing", "pow"]
+  retrieve_array = ["am","zing", "pow"]
   retrieve_array[0]
 end
 
