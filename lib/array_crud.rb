5fb13a389ba08f48784f546778f4a3753f93b9d0
Harrison Price <hprice518@gmail.com>
2020-04-23 00:27:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array(create_an_array, "Elton John")
-  create_an_array.push
+  create_an_array.push("Elton John")
 end
 
 def add_element_to_start_of_array(array, element)
